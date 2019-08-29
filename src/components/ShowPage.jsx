ea6ab5406389f4b8f2a6e43df991362e34ad6f5a
nickryanweber <nicholasryanweber@gmail.com>
2019-08-29 18:36:17 -0400

    added API call

--- src/components/ShowPage.jsx
+++ src/components/ShowPage.jsx
@@ -1,35 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css'
+import axios from 'axios'
 
 const ShowPage = () => {
+  const fetchTvData = async () => {
+    const resp = await axios.get(
+      'https://api.themoviedb.org/3/tv/60625/credits?api_key=03423e5f93610593731b95e4756610af&language=en-US'
+    )
+    console.log(resp.data, 'Rick and Morty')
+  }
+
   useEffect(() => {
     M.AutoInit()
+    fetchTvData()
   }, [])
 
   return (
     <>
       <main className="container">
         <div className="row">
-          <div className="col l6 offset-l3">
+          <div className="col l5">
             <img
-              className="show-image col l6"
+              className="show-image"
               src="https://cdn11.bigcommerce.com/s-nq6l4syi/images/stencil/1280x1280/products/5358/5846/3015-1024__28352.1452060019.jpg?c=2?imbypass=on"
               alt=""
             />
-            <h2 className="center">Show Title</h2>
-            <section className="row">
-              <ul className="col l6">
-                <li>Chris Hayes</li>
-                <li>Nick Weber</li>
-              </ul>
-              <p className="col l6">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-                molestiae excepturi eos quibusdam illo! Totam blanditiis fugit,
-                molestias quos quibusdam quia? Dicta perferendis officia
-                expedita omnis inventore repudiandae nemo cupiditate.
-              </p>
-            </section>
+          </div>
+          <div className="col l7">
+            <h2 className="left-align">Show Title</h2>
+            <h5>Cast</h5>
+            <ul>
+              <li>Chris Hayes</li>
+              <li>Nick Weber</li>
+            </ul>
+            <h5>Show Description</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+              molestiae excepturi eos quibusdam illo! Totam blanditiis fugit,
+              molestias quos quibusdam quia? Dicta perferendis officia expedita
+              omnis inventore repudiandae nemo cupiditate.
+            </p>
           </div>
         </div>
       </main>
