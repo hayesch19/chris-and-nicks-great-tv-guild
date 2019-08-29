8172c74f869017adb25427ed7129a0f6deb73cc8
nickryanweber <nicholasryanweber@gmail.com>
2019-08-29 16:51:34 -0400

    added custom css for show image height

--- src/components/ShowPage.jsx
+++ src/components/ShowPage.jsx
@@ -7,7 +7,21 @@ const ShowPage = () => {
     M.AutoInit()
   }, [])
 
-  return <div>This Is A Show Page</div>
+  return (
+    <>
+      <main className="container">
+        <div className="row">
+          <div className="col l4 offset-l2">
+            <img
+              className="show-image"
+              src="https://cdn11.bigcommerce.com/s-nq6l4syi/images/stencil/1280x1280/products/5358/5846/3015-1024__28352.1452060019.jpg?c=2?imbypass=on"
+              alt=""
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default ShowPage
