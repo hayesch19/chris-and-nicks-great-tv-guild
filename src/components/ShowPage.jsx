1c2ae661732f9c91562c35aa2ae6b93689aabe7c
nickryanweber <nicholasryanweber@gmail.com>
2019-08-29 16:57:50 -0400

    added image and title to show page

--- src/components/ShowPage.jsx
+++ src/components/ShowPage.jsx
@@ -11,12 +11,13 @@ const ShowPage = () => {
     <>
       <main className="container">
         <div className="row">
-          <div className="col l4 offset-l2">
+          <div className="col l4 offset-l4">
             <img
               className="show-image"
               src="https://cdn11.bigcommerce.com/s-nq6l4syi/images/stencil/1280x1280/products/5358/5846/3015-1024__28352.1452060019.jpg?c=2?imbypass=on"
               alt=""
             />
+            <h2 className="center">Show Title</h2>
           </div>
         </div>
       </main>
