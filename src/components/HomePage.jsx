efe1f7f75b38a0baa8cef69a86088c4c5921a2ad
hayesch19 <chrishayespro@gmail.com>
2019-09-01 15:03:28 -0400

    Random show display added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -6,18 +6,18 @@ import axios from 'axios'
 
 const HomePage = () => {
   const [tvShows, setTvShows] = useState([])
-  const [randomShow, setRandomShow] = useState([0])
+  const [randomShows, setRandomShows] = useState([0])
 
   const fetchData = async () => {
     const resp = await axios.get(
       'https://api.themoviedb.org/3/tv/top_rated?api_key=03423e5f93610593731b95e4756610af&language=en-US&page=1'
     )
     setTvShows(resp.data.results)
-    setRandomShow(
+    setRandomShows(
       resp.data.results[Math.floor(Math.random) * resp.data.results.length - 1]
     )
     console.log(resp.data, 'Shows')
-    console.log(randomShow, 'Random')
+    console.log(resp.data, 'Random')
   }
 
   useEffect(() => {
@@ -30,17 +30,23 @@ const HomePage = () => {
       <main className="container">
         <section className="hero-show">
           <div className="row">
-            <div className="col s12 offset-s2">
-              <img
-                className="col s3 hero-image"
-                src="https://cdn11.bigcommerce.com/s-nq6l4syi/images/stencil/1280x1280/products/5358/5846/3015-1024__28352.1452060019.jpg?c=2?imbypass=on"
-                alt=""
-              />
-              <section className="col s6">
-                <h3>Title</h3>
-                <p> Overview</p>
-              </section>
-            </div>
+            {randomShows.map((randomShow, i) => {
+              return (
+                <div key={i} className="col s12 offset-s2">
+                  <img
+                    className="col s3 hero-image"
+                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${randomShow.poster_path}`}
+                    alt=""
+                  />
+                  <section className="col s6">
+                    <h3>{randomShow.name}</h3>
+                    <p>Rating: {randomShow.vote_average}</p>
+                    <p>First Aired: {randomShow.first_air_date}</p>
+                    <p> {randomShow.overview}</p>
+                  </section>
+                </div>
+              )
+            })}
           </div>
         </section>
         <section className="show-list">
@@ -63,7 +69,7 @@ const HomePage = () => {
                     </Link>
                   </div>
                   <section className="col s6">
-                    <h3>{tvShow.name}</h3>
+                    <h5>{tvShow.name}</h5>
                     <p>Rating: {tvShow.vote_average}</p>
                     <p>First Aired: {tvShow.first_air_date}</p>
                     <p>{tvShow.overview}</p>
